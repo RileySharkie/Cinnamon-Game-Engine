10fa50a6599c704e367f62956398e3d9a5ac1497
ntuni <38499841+ntuni@users.noreply.github.com>
2019-11-10 16:40:31 -0500

    Update architect.py

--- architect.py
+++ architect.py
@@ -3,6 +3,21 @@ import room as r
 import entity
 
 
+def architect(room):
+    grid = 32
+    with open('assets/rooms/' + room + '.txt', 'r') as file:
+        data = file.read().replace('\n', '')
+    for line in data:
+        line = line.split(' ')
+    entity_list = []
+    xpos, ypos = 0, 0
+    for line in data:
 
-class architect:
-    
+        for entry in line:
+            if entry != 0:
+                entity_list.append((entry, (xpos, ypos)))
+            xpos += grid
+        xpos = 0
+        ypos += grid
+    return entity_list
+
